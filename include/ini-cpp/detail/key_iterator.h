b7477be129c1505873717ea14b647d3bb7b01c45
Ark1409 <codeark1409@gmail.com>
2023-07-05 22:37:44 -0700

    Fixed pair_type in key_iterator

--- include/ini-cpp/detail/key_iterator.h
+++ include/ini-cpp/detail/key_iterator.h
@@ -20,7 +20,8 @@ namespace inicpp {
         public:
             typedef std::remove_reference_t<K> key_type;
             typedef std::remove_reference_t<V> value_type;
-            typedef std::pair<key_type const, value_type> pair_type;
+            typedef std::conditional_t<std::is_const_v<value_type>, std::pair<key_type const, std::remove_const_t<value_type>> const,
+                std::pair<key_type const, std::remove_const_t<value_type>>> pair_type;
         private:
             typedef detail::ordered_map<key_type, std::remove_const_t<value_type>> impl_base_map;
             typedef std::conditional_t<std::is_const_v<value_type>, const impl_base_map, impl_base_map> impl_map;
@@ -39,11 +40,8 @@ namespace inicpp {
             key_iterator_type& operator=(const key_iterator_type&) noexcept = default;
             key_iterator_type& operator=(key_iterator_type&&) noexcept = default;
 
-            inline pair_type& operator*() noexcept { return *m_cur; }
-            inline pair_type const& operator*() const noexcept { return *m_cur; }
-
-            inline pair_type* operator->() noexcept { return m_cur.operator->(); }
-            inline pair_type const* operator->() const noexcept { return m_cur.operator->(); }
+            inline pair_type& operator*() const noexcept { return *m_cur; }
+            inline pair_type* operator->() const noexcept { return m_cur.operator->(); }
 
             key_iterator_type& operator++() noexcept(std::is_const_v<value_type>) {
                 if (m_map) {
@@ -111,11 +109,8 @@ namespace inicpp {
             inline bool operator==(const reverse_key_iterator_type& other) const noexcept { return this->m_it == other.m_it; }
             inline bool operator!=(const reverse_key_iterator_type& other) const noexcept { return !operator==(other); }
 
-            inline pair_type& operator*() noexcept { auto tmp = m_it; return (--tmp).operator *(); }
-            inline pair_type const& operator*() const noexcept { auto tmp = m_it; return (--tmp).operator *(); }
-
-            inline pair_type* operator->() noexcept { auto tmp = m_it; return (--tmp).operator->(); }
-            inline pair_type const* operator->() const noexcept { auto tmp = m_it; return (--tmp).operator->(); }
+            inline pair_type& operator*() const noexcept { auto tmp = m_it; return (--tmp).operator *(); }
+            inline pair_type* operator->() const noexcept { auto tmp = m_it; return (--tmp).operator->(); }
 
             inline reverse_key_iterator_type& operator++() noexcept(noexcept(key_iterator_type::operator++())) { --m_it; return *this; }
             inline reverse_key_iterator_type operator++(int) noexcept(noexcept(key_iterator_type::operator++(int(0)))) { auto ret = m_it--; return reverse_key_iterator(ret); }
